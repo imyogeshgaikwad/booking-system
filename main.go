9370f8b73020f975ad7189d3f9c92032f94b5efa
BEAST <imyogeshgaikwad@gmail.com>
2026-03-24 20:37:05 +0000

    Done with the multiple tickets generator

--- main.go
+++ main.go
@@ -1,20 +1,58 @@
 package main
-import "fmt"
 
-func main (){
-	var conferenceName = "Go conference"
-	const conferenceTickets = 50
-	var remainingTickets = 50
+import (
+	"fmt"
+	"strings"
+)
 
-	fmt.Println("Welcome to",conferenceName,"booking application")
-	fmt.Println ("We have total of",conferenceTickets,"tickets and",remainingTickets,"are still available")
+func main() {
+	conferenceName := "Go conference"
+	const conferenceTickets int = 50
+	var remainingTickets uint = 50
+	bookings := []string{}
+
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend the conference")
 
+	for {
 
-	
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint
+		//ask user for name
+		fmt.Println("Enter your first name: ")
+		fmt.Scan(&firstName)
 
+		fmt.Println("Enter your last name: ")
+		fmt.Scan(&lastName)
 
+		fmt.Println("Enter your email address: ")
+		fmt.Scan(&email)
 
+		fmt.Println("How many tickets you want: ")
+		fmt.Scan(&userTickets)
 
+		remainingTickets = remainingTickets - userTickets
+		bookings = append(bookings, firstName+" "+lastName)
+
+		fmt.Printf("The whole slice : %v\n", bookings)
+		fmt.Printf("The first value : %v\n", bookings[0])
+		fmt.Printf("The type : %T\n", bookings)
+		fmt.Printf("Slice length: %v\n", len(bookings))
+
+		fmt.Printf("Thank you %v %v for booking %v tickets. you will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+		fmt.Printf("%v tickets are remaining for %v", remainingTickets, conferenceName)
+
+		firstNames := []string{}
+		for _, booking := range bookings {
+			var names = strings.Fields(booking)
+			firstNames = append(firstNames, names[0])
+
+		}
+		fmt.Printf("The first names of bookings are: %v\n", firstNames)
+
+	}
 
 }
