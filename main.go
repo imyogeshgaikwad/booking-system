1cebe2557dd9f49fa0036302270da00adae39450
BEAST <imyogeshgaikwad@gmail.com>
2026-03-24 22:56:35 +0000

    Done with looping function logic

--- main.go
+++ main.go
@@ -11,9 +11,7 @@ func main() {
 	var remainingTickets uint = 50
 	bookings := []string{}
 
-	fmt.Printf("Welcome to %v booking application\n", conferenceName)
-	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
-	fmt.Println("Get your tickets here to attend the conference")
+	greetUsers(conferenceName, conferenceTickets, remainingTickets)
 
 	for {
 
@@ -21,7 +19,7 @@ func main() {
 		var lastName string
 		var email string
 		var userTickets uint
-		//ask user for name
+
 		fmt.Println("Enter your first name: ")
 		fmt.Scan(&firstName)
 
@@ -34,25 +32,63 @@ func main() {
 		fmt.Println("How many tickets you want: ")
 		fmt.Scan(&userTickets)
 
-		remainingTickets = remainingTickets - userTickets
-		bookings = append(bookings, firstName+" "+lastName)
+		isValidName, isValidEmail, isValidTicketNumber :=
+			validateUserInput(firstName, lastName, email, userTickets, remainingTickets)
 
-		fmt.Printf("The whole slice : %v\n", bookings)
-		fmt.Printf("The first value : %v\n", bookings[0])
-		fmt.Printf("The type : %T\n", bookings)
-		fmt.Printf("Slice length: %v\n", len(bookings))
+		if isValidName && isValidEmail && isValidTicketNumber {
 
-		fmt.Printf("Thank you %v %v for booking %v tickets. you will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
-		fmt.Printf("%v tickets are remaining for %v", remainingTickets, conferenceName)
+			remainingTickets -= userTickets
+			bookings = append(bookings, firstName+" "+lastName)
 
-		firstNames := []string{}
-		for _, booking := range bookings {
-			var names = strings.Fields(booking)
-			firstNames = append(firstNames, names[0])
+			fmt.Printf("Thank you %v %v for booking %v tickets.\n", firstName, lastName, userTickets)
+			fmt.Printf("You will receive a confirmation email at %v\n", email)
+			fmt.Printf("%v tickets are remaining for %v\n", remainingTickets, conferenceName)
 
+			fmt.Printf("The whole slice: %v\n", bookings)
+			fmt.Printf("Slice length: %v\n", len(bookings))
+
+			firstNames := getFirstNames(bookings)
+			fmt.Printf("First names: %v\n", firstNames)
+
+			if remainingTickets == 0 {
+				fmt.Println("Our conference is booked out. Come back next year.")
+				break
+			}
+
+		} else {
+			if !isValidName {
+				fmt.Println("First name or last name is too short")
+			}
+			if !isValidEmail {
+				fmt.Println("Email must contain @")
+			}
+			if !isValidTicketNumber {
+				fmt.Println("Invalid number of tickets")
+			}
 		}
-		fmt.Printf("The first names of bookings are: %v\n", firstNames)
-
 	}
+}
 
+func greetUsers(confName string, confTickets int, remainTickets uint) {
+	fmt.Printf("Welcome to %v\n", confName)
+	fmt.Printf("Total tickets: %v, Remaining: %v\n", confTickets, remainTickets)
+	fmt.Println("Get your tickets here!")
+}
+
+func getFirstNames(bookings []string) []string {
+	firstNames := []string{}
+
+	for _, booking := range bookings {
+		names := strings.Fields(booking)
+		firstNames = append(firstNames, names[0])
+	}
+	return firstNames
+}
+
+func validateUserInput(firstName string, lastName string, email string, userTickets uint, remainingTickets uint) (bool, bool, bool) {
+	isValidName := len(firstName) >= 2 && len(lastName) >= 2
+	isValidEmail := strings.Contains(email, "@")
+	isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
+
+	return isValidName, isValidEmail, isValidTicketNumber
 }
